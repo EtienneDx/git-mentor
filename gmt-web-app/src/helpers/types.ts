5d2d9685e7183952f5c81ebb8e00974f4abc3efb
Lorne Francois <francois.lorne@student-cs.fr>
2023-12-21 15:58:56 +0100

    feat: create data viz pages

---
+++ gmt-web-app/src/helpers/types.ts
@@ -0,0 +1,17 @@
+export type Group = {
+  name: string;
+  id: string;
+  creationDate: Date;
+};
+
+export type Repository = {
+  name: string;
+  id: string;
+  creationDate: Date;
+};
+
+export type Student = {
+  name: string;
+  id: string;
+  creationDate: Date;
+};
