333ede7edaf64e1af75f09b8dfff05d31cf1caa9
Etienne Dx <etiennedx00@gmail.com>
2024-02-26 15:20:33 +0100

    test: added service test

--- gmt-api/src/services/hello_service.rs
+++ gmt-api/src/services/hello_service.rs
@@ -5,12 +5,12 @@ use poem_openapi::{
 
 pub struct HelloService;
 
-#[derive(Object, serde::Deserialize)]
+#[derive(Object, serde::Serialize, serde::Deserialize)]
 struct HelloRequest {
   name: String,
 }
 
-#[derive(Object, serde::Serialize)]
+#[derive(Object, serde::Serialize, serde::Deserialize)]
 struct HelloResponse {
   message: String,
 }
@@ -27,5 +27,58 @@ impl HelloService {
     Json(HelloResponse {
       message: format!("Hello, {}!", req.name),
     })
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use poem::{http::StatusCode, test::TestClient, Route};
+  use poem_openapi::OpenApiService;
+  use rstest::{fixture, rstest};
+
+  #[fixture]
+  fn client() -> TestClient<Route> {
+    let service = OpenApiService::new(HelloService, "", "");
+
+    let app = Route::new().nest("/", service);
+
+    TestClient::new(app)
+  }
+
+  #[rstest]
+  #[tokio::test]
+  async fn test_hello() {
+    let service = OpenApiService::new(HelloService, "", "");
+
+    let app = Route::new().nest("/", service);
+
+    let client = TestClient::new(app);
+    let resp = client.get("/hello").send().await;
+    resp.assert_status(StatusCode::OK);
+    resp.assert_text("Hello world!").await;
+  }
+
+  #[rstest]
+  #[tokio::test]
+  async fn test_hello_name() {
+    let service = OpenApiService::new(HelloService, "", "");
+
+    let app = Route::new().nest("/", service);
+
+    let client = TestClient::new(app);
+    let resp = client
+      .post("/hello")
+      .body_json(&HelloRequest {
+        name: "world".to_string(),
+      })
+      .send()
+      .await;
+    resp.assert_status(StatusCode::OK);
+    resp
+      .assert_json(HelloResponse {
+        message: "Hello, world!".to_string(),
+      })
+      .await;
   }
 }
