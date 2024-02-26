d97dd6eeea5a7ec90b9b9ea492dfe9c856a40961
EtienneDx <32039535+EtienneDx@users.noreply.github.com>
2024-02-26 12:04:32 +0100

    fix: Changed service name

--- gmt-api/src/services/mod.rs
+++ gmt-api/src/services/mod.rs
@@ -5,5 +5,5 @@ use self::hello_service::HelloService;
 pub mod hello_service;
 
 pub fn make_service() -> OpenApiService<HelloService, ()> {
-  OpenApiService::new(HelloService, "Calculator", "1.0")
+  OpenApiService::new(HelloService, "Git Mentor APIs", "1.0")
 }
