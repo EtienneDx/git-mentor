c33dd935fa2f501ebad3231f639ceb16075561c2
Etienne Dx <etiennedx00@gmail.com>
2024-03-28 12:45:30 +0100

    test: usertoken test

--- gmt-api/src/services/auth_service/user_token.rs
+++ gmt-api/src/services/auth_service/user_token.rs
@@ -25,3 +25,28 @@ impl From<User> for UserToken {
     }
   }
 }
+
+#[cfg(test)]
+mod tests {
+  use database::db_handle::user::User;
+
+  use super::*;
+
+  #[test]
+  fn test_from_user_to_user_token() {
+    let user = User {
+      id: 1,
+      username: "john_doe".to_string(),
+      password: "password".to_string(),
+      email: "john.doe@example.com".to_string(),
+      pubkey: vec![Some("key1".to_string()), None, Some("key2".to_string())],
+    };
+
+    let user_token: UserToken = user.into();
+
+    assert_eq!(user_token.user_id, 1);
+    assert_eq!(user_token.username, "john_doe");
+    assert_eq!(user_token.email, "john.doe@example.com");
+    assert_eq!(user_token.pubkeys, vec!["key1", "key2"]);
+  }
+}
