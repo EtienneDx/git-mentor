b6c0dd7e714b05dd9066d7c9615c8bf5045fa751
Etienne Dx <etiennedx00@gmail.com>
2024-03-28 12:38:23 +0100

    test: added password hash tests

--- gmt-common/src/password.rs
+++ gmt-common/src/password.rs
@@ -19,3 +19,31 @@ impl PasswordAuth for PasswordAuthImpl {
     true
   }
 }
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_generate_hash() {
+    let password = "password";
+    let hash = PasswordAuthImpl::generate_hash(password);
+    assert_eq!(hash.len(), "$argon2id$v=19$m=19456,t=2,p=1$KmhCTks8Qx0nfomuy+AHRw$IlnT6WL3hBzR2gHsWeE7vAr9ccprFP5a43GFi1WJEcM".len());
+  }
+
+  #[test]
+  fn test_verify_password() {
+    let password = "password";
+    let hash = "$argon2id$v=19$m=19456,t=2,p=1$KmhCTks8Qx0nfomuy+AHRw$IlnT6WL3hBzR2gHsWeE7vAr9ccprFP5a43GFi1WJEcM";
+    let result = PasswordAuthImpl::verify_password(password, hash);
+    assert!(result, "Password should be verified");
+  }
+
+  #[test]
+  fn test_verify_password_fail() {
+    let password = "not password";
+    let hash = "$argon2id$v=19$m=19456,t=2,p=1$KmhCTks8Qx0nfomuy+AHRw$IlnT6WL3hBzR2gHsWeE7vAr9ccprFP5a43GFi1WJEcM";
+    let result = PasswordAuthImpl::verify_password(password, hash);
+    assert!(!result, "Password should not be verified");
+  }
+}
